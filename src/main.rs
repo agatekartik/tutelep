177c29ca5b8306047b81ba17e38f2f843ee8321f
Kartikeya Agate <agatekartik@gmail.com>
2024-07-03 16:02:06 +0530

    chap 3.2: Dynamic Classes

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn App() -> impl IntoView {
                 // on nightly this is set_count(3);
                 set_count.update(|n| *n += 1);
             }
+            class:red=move || count.get() % 2 == 1
         >
             "Click me: "
             // on nightly this is {move || count()};
