eab968c0ecc7919424ed9f5a98c5c45e9aaf11b5
Kartikeya Agate <agatekartik@gmail.com>
2024-07-03 15:27:59 +0530

    chapter 3.1: A basic component

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,23 @@ use leptos::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(|| view! { <App /> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click = move |_| {
+                // on nightly this is set_count(3);
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            // on nightly this is {move || count()};
+            {move || count.get()}
+        </button>
+    }
 }
