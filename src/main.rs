9052c32da870169cc492b98ef0abedfe3709ed83
Kartikeya Agate <agatekartik@gmail.com>
2024-07-06 15:28:06 +0530

    Chap 3.3 using into in derived signal

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,34 @@ fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| view! { <App /> })
 }
-
 #[component]
-fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+fn ProgressBar(
+    #[prop(default = 100)]
+    max: u16,
+    #[prop(into)]
+    progress: Signal<i32>
+) -> impl IntoView
+{
     view! {
-        <button on:click=move |_| { set_count.update(|n| *n += 1); }>
-            "Click me"
-        </button>
-        // now we use our component!
-        <ProgressBar progress=count/>
+        <progress
+            max=max
+            value=progress
+        />
     }
 }
 
 #[component]
-fn ProgressBar(
-    progress: ReadSignal<i32>
-) -> impl IntoView {
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    let double_count = move || count.get() * 2;
+
     view! {
-        <progress
-            max="50"
-            // now this works
-            value=progress
-        />
+        <button on:click=move |_| { set_count.update(|n| *n += 1); }>
+            "Click me"
+        </button>
+        // .into() converts `ReadSignal` to `Signal`
+        <ProgressBar progress=count/>
+        // use `Signal::derive()` to wrap a derived signal
+        <ProgressBar progress=Signal::derive(double_count)/>
     }
 }
