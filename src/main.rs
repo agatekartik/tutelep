5d8c38fd3e209821ca9274f0a60a4cbed1f7cedf
Kartikeya Agate <agatekartik@gmail.com>
2024-07-06 15:24:05 +0530

    chap 3.3

--- src/main.rs
+++ src/main.rs
@@ -8,41 +8,24 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
-    let (x, set_x) = create_signal(0);
-    
     view! {
-        <button
-            on:click = move |_| {
-                // on nightly this is set_count(3);
-                set_count.update(|n| *n += 1);
-            }
-            class:red=move || count.get() % 2 == 1
-            // class=("button-20", move || count.get()%2 == 1)
-        >
-            "Click me: "
-            // on nightly this is {move || count()};
-            {move || count}
+        <button on:click=move |_| { set_count.update(|n| *n += 1); }>
+            "Click me"
         </button>
-
-        <button
-            on:click={move |_| {
-                set_x.update(|n| *n += 10);
-            }}
-            // set the `style` attribute
-            style="position: absolute"
-            // and toggle individual CSS properties with `style:`
-            style:left=move || format!("{}px", x.get() + 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", x.get(), 100)
-            style:max-width="400px"
-            // Set a CSS variable for stylesheet use
-            style=("--columns", x)
-        >
-            "Click to Move"
-        </button>
-        <progress max="50"
-        // signals are functions, so `value=count` and `value=move || count.get()`
-        // are interchangeable.
-        value=count
-        />
+        // now we use our component!
+        <ProgressBar progress=count/>
+    }
 }
+
+#[component]
+fn ProgressBar(
+    progress: ReadSignal<i32>
+) -> impl IntoView {
+    view! {
+        <progress
+            max="50"
+            // now this works
+            value=progress
+        />
+    }
 }
